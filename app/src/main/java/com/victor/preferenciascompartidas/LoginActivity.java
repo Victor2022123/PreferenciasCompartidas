a36b2d5fa930134af5ad1e659748bd8c3bc12907
victor <vise.c2@gmail.com>
2023-10-22 15:00:34 -0300

    Implementacion Finalizada de Preferencias Compartidas

--- app/src/main/java/com/victor/preferenciascompartidas/LoginActivity.java
+++ app/src/main/java/com/victor/preferenciascompartidas/LoginActivity.java
@@ -93,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
 
         } else {
             if (checkBoxRecordar.isChecked())
-                salvarLoginDetalles(email, password);
+                    salvarLoginDetalles(email, password);
                 startHomeActivity();
 
 
